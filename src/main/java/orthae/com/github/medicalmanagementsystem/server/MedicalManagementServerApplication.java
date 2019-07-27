d966d51305efa1f2e7d7dd0b1cf0857c80b79fd7
Orthae <orthaefluorite@gmail.com>
2019-07-27 12:56:25 +0200

    init

---
+++ src/main/java/orthae/com/github/medicalmanagementsystem/server/MedicalManagementServerApplication.java
@@ -0,0 +1,13 @@
+package orthae.com.github.medicalmanagementsystem.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalManagementServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalManagementServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/orthae/com/github/medicalmanagementsystem/server/MedicalManagementServerApplicationTests.java
@@ -0,0 +1,13 @@
+package orthae.com.github.medicalmanagementsystem.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalManagementServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
