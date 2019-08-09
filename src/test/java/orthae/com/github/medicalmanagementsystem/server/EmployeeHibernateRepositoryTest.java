146273c1ec2f480ca4f48f64a11b76a643edd084
Orthae <orthaefluorite@gmail.com>
2019-08-09 22:00:40 +0200

    param reorder

--- src/test/java/orthae/com/github/medicalmanagementsystem/server/EmployeeHibernateRepositoryTest.java
+++ src/test/java/orthae/com/github/medicalmanagementsystem/server/EmployeeHibernateRepositoryTest.java
@@ -33,28 +33,29 @@ class EmployeeHibernateRepositoryTest {
         assertEquals(list.size(), 25);
 
         Employee employee = list.get(0);
-        assertEquals(employee.getName(), "Daniel");
-        assertEquals(employee.getSurname(), "Bayne");
-        assertEquals(employee.getUsername(), "baydan");
-        assertEquals(employee.getPassword(), "$2y$05$L8IQDO993A5f/G/z7VjHm.XFwg4rdPCdUkTr/oTa0HaHSMPFQf9fu");
-        assertEquals(employee.getAuthorities().size(), 2);
+        assertEquals("Daniel", employee.getName());
+        assertEquals("Bayne", employee.getSurname());
+        assertEquals("baydan", employee.getUsername());
+        assertTrue(passwordEncoder.matches("baydan", employee.getPassword()));
+        assertEquals(2, employee.getAuthorities().size());
         List<GrantedAuthority> baydanAuth = new ArrayList<>(employee.getAuthorities());
-        assertEquals(baydanAuth.get(0).getAuthority(), "ROLE_ADMIN");
-        assertEquals(baydanAuth.get(1).getAuthority(), "ROLE_USER");
+        assertEquals("ROLE_ADMIN", baydanAuth.get(0).getAuthority());
+        assertEquals("ROLE_USER", baydanAuth.get(1).getAuthority());
 
         employee = list.get(9);
-        assertEquals(employee.getName(), "Jana");
-        assertEquals(employee.getSurname(), "Williamson");
-        assertEquals(employee.getUsername(), "wiljan");
-        assertEquals(employee.getPassword(), "$2y$05$fYs1xCi72853IBiDnz/veOsqtnGa/OlDjP0zYbQMq5vfEZsaiEJGi");
-        assertEquals(employee.getAuthorities().size(), 0);
+        assertEquals("Jana", employee.getName());
+        assertEquals("Williamson", employee.getSurname());
+        assertEquals("wiljan", employee.getUsername());
+        assertTrue(passwordEncoder.matches("wiljan", employee.getPassword()));
+        assertEquals("$2y$05$fYs1xCi72853IBiDnz/veOsqtnGa/OlDjP0zYbQMq5vfEZsaiEJGi", employee.getPassword());
+        assertEquals(0, employee.getAuthorities().size());
 
         employee = list.get(22);
-        assertEquals(employee.getName(), "Rocco");
-        assertEquals(employee.getSurname(), "Bowden");
-        assertEquals(employee.getUsername(), "bowroc");
-        assertEquals(employee.getPassword(), "$2y$05$6dg1EmZ0as6IatdCyAJNpuIgcOdDCKcfYtc7EGkKviA4RBqX/zE06");
-        assertEquals(employee.getAuthorities().size(), 0);
+        assertEquals("Rocco", employee.getName());
+        assertEquals("Bowden", employee.getSurname());
+        assertEquals("bowroc", employee.getUsername());
+        assertTrue(passwordEncoder.matches("bowroc", employee.getPassword()));
+        assertEquals(0, employee.getAuthorities().size());
 
 
 //  TODO ADD ONE MORE
@@ -64,23 +65,23 @@ class EmployeeHibernateRepositoryTest {
     @Test
     void findEmployeeById() {
         Employee employee = employeeRepository.find(1);
-        assertEquals(employee.getName(), "Daniel");
-        assertEquals(employee.getSurname(), "Bayne");
-        assertEquals(employee.getUsername(), "baydan");
-        assertEquals(employee.getPassword(), "$2y$05$L8IQDO993A5f/G/z7VjHm.XFwg4rdPCdUkTr/oTa0HaHSMPFQf9fu");
-        assertEquals(employee.getAuthorities().size(), 2);
+        assertEquals("Daniel", employee.getName());
+        assertEquals("Bayne", employee.getSurname());
+        assertEquals("baydan", employee.getUsername());
+        assertTrue(passwordEncoder.matches("baydan", employee.getPassword()));
+        assertEquals(2, employee.getAuthorities().size());
         List<GrantedAuthority> baydanAuth = new ArrayList<>(employee.getAuthorities());
-        assertEquals(baydanAuth.get(0).getAuthority(), "ROLE_ADMIN");
-        assertEquals(baydanAuth.get(1).getAuthority(), "ROLE_USER");
+        assertEquals("ROLE_ADMIN", baydanAuth.get(0).getAuthority());
+        assertEquals("ROLE_USER", baydanAuth.get(1).getAuthority());
     }
 
     @Test
     void findEmployeeByIdTwo() {
         Employee employee = employeeRepository.find(2);
-        assertEquals(employee.getName(), "Richard");
-        assertEquals(employee.getSurname(), "Morris");
-        assertEquals(employee.getUsername(), "morric");
-        assertEquals(employee.getPassword(), "$2y$05$dDh/KjkFg7qBdiYUq.haEuH0dmnlIoGg3d7hGaZklEk.BETns0Ma6");
+        assertEquals("Richard", employee.getName());
+        assertEquals("Morris", employee.getSurname());
+        assertEquals("morric", employee.getUsername());
+        assertTrue(passwordEncoder.matches("morric", employee.getPassword()));
     }
 
     @Test
@@ -92,40 +93,40 @@ class EmployeeHibernateRepositoryTest {
     @Test
     void findByNameNoMatch(){
         List<Employee> list = employeeRepository.find("There is no such name", null);
-        assertEquals(list.size(), 0);
+        assertEquals(0, list.size());
     }
 
     @Test
     void findByNameOneMatch(){
         List<Employee> list = employeeRepository.find("Chelsy", null);
-        assertEquals(list.size(), 1);
+        assertEquals(1, list.size());
 
         Employee employee = list.get(0);
-        assertEquals(employee.getName(), "Chelsy");
-        assertEquals(employee.getSurname(), "Lacey");
-        assertEquals(employee.getUsername(), "lacche");
-        assertEquals(employee.getPassword(), "$2y$05$MQXAEsD48oQPNiwTx1l8S.tI8sfLdeInk2eym2MnIVHzukRXdJxK2");
-        assertEquals(employee.getAuthorities().size(), 0);
+        assertEquals("Chelsy", employee.getName());
+        assertEquals("Lacey", employee.getSurname());
+        assertEquals("lacche", employee.getUsername());
+        assertTrue(passwordEncoder.matches("lacche", employee.getPassword()));
+        assertEquals(0, employee.getAuthorities().size());
     }
 
     @Test
     void findByNameMultiMatch(){
         List<Employee> list = employeeRepository.find("Oliver", null);
-        assertEquals(list.size(), 2);
+        assertEquals(2, list.size());
 
         Employee employee = list.get(0);
-        assertEquals(employee.getName(), "Oliver");
-        assertEquals(employee.getSurname(), "Weaver");
-        assertEquals(employee.getUsername(), "weaoli");
-        assertEquals(employee.getPassword(), "$2y$05$TmdTZBHyZzLVSHJHiSRntuHGnJMBkbLjsNKR1UIlKJKZ.XWMXwiBO");
-        assertEquals(employee.getAuthorities().size(), 0);
+        assertEquals("Oliver", employee.getName());
+        assertEquals("Weaver", employee.getSurname());
+        assertEquals("weaoli", employee.getUsername());
+        assertTrue(passwordEncoder.matches("weaoli", employee.getPassword()));
+        assertEquals(0, employee.getAuthorities().size());
 
         employee = list.get(1);
-        assertEquals(employee.getName(), "Oliver");
-        assertEquals(employee.getSurname(), "Kunal");
-        assertEquals(employee.getUsername(), "kunoli");
-        assertEquals(employee.getPassword(), "$2y$05$AyxkcHxUFMa.1J4my92OLOQlIJDxWly1CYo8vOtK1QqT6Wa1bg5CG");
-        assertEquals(employee.getAuthorities().size(), 0);
+        assertEquals("Oliver", employee.getName());
+        assertEquals("Kunal", employee.getSurname());
+        assertEquals("kunoli", employee.getUsername());
+        assertTrue(passwordEncoder.matches("kunoli", employee.getPassword()));
+        assertEquals(0, employee.getAuthorities().size());
     }
 
     @Test
@@ -139,11 +140,11 @@ class EmployeeHibernateRepositoryTest {
         List<Employee> list = employeeRepository.find(null, "Salinas");
         assertEquals(list.size(), 1);
         Employee employee = list.get(0);
-        assertEquals(employee.getName(), "Caitlin");
-        assertEquals(employee.getSurname(), "Salinas");
-        assertEquals(employee.getUsername(), "salcai");
-        assertEquals(employee.getPassword(), "$2y$05$2QfAwCUZaQD7LKrSgzN1.OREG2ksBkOXGmdHVPsVUQILjCsAXYrzC");
-        assertEquals(employee.getAuthorities().size(), 0);
+        assertEquals("Caitlin", employee.getName());
+        assertEquals("Salinas", employee.getSurname());
+        assertEquals("salcai", employee.getUsername());
+        assertTrue(passwordEncoder.matches("salcai", employee.getPassword()));
+        assertEquals(0, employee.getAuthorities().size());
 
     }
 
@@ -153,18 +154,18 @@ class EmployeeHibernateRepositoryTest {
         assertEquals(list.size(), 2);
 
         Employee employee = list.get(0);
-        assertEquals(employee.getName(), "Leon");
-        assertEquals(employee.getSurname(), "Meyers");
-        assertEquals(employee.getUsername(), "meyleo");
-        assertEquals(employee.getPassword(), "$2y$05$TjdhULMjfV1bwjx67pzrjuGc7S1gAmtMQtY1pMosjoWqmslCJmPy2");
-        assertEquals(employee.getAuthorities().size(), 2);
+        assertEquals("Leon", employee.getName());
+        assertEquals("Meyers", employee.getSurname());
+        assertEquals("meyleo", employee.getUsername());
+        assertTrue(passwordEncoder.matches("meyleo", employee.getPassword()));
+        assertEquals(2, employee.getAuthorities().size());
 
         employee = list.get(1);
-        assertEquals(employee.getName(), "Nicholas");
-        assertEquals(employee.getSurname(), "Meyers");
-        assertEquals(employee.getUsername(), "meynic");
-        assertEquals(employee.getPassword(), "$2y$05$fyNfpRs.P8C4g64E6ba9DeCvU8jXpKxycUxVEGTqB52RT0WIr6.5q");
-        assertEquals(employee.getAuthorities().size(), 0);
+        assertEquals("Nicholas", employee.getName());
+        assertEquals("Meyers", employee.getSurname());
+        assertEquals("meynic", employee.getUsername());
+        assertTrue(passwordEncoder.matches("meynic", employee.getPassword()));
+        assertEquals(0, employee.getAuthorities().size());
     }
 
     @Test
@@ -179,11 +180,11 @@ class EmployeeHibernateRepositoryTest {
         assertEquals(list.size(), 1);
 
         Employee employee = list.get(0);
-        assertEquals(employee.getName(), "Hania");
-        assertEquals(employee.getSurname(), "Black");
-        assertEquals(employee.getUsername(), "blahan");
-        assertEquals(employee.getPassword(), "$2y$05$xqjHh3NpsRcWEYesvkiWueSLU7ARMQl2OTHR6lDOkbPCawEkVhYSy");
-        assertEquals(employee.getAuthorities().size(), 0);
+        assertEquals("Hania", employee.getName());
+        assertEquals("Black", employee.getSurname());
+        assertEquals("blahan", employee.getUsername());
+        assertTrue(passwordEncoder.matches("blahan", employee.getPassword()));
+        assertEquals(0, employee.getAuthorities().size());
 
     }
 
@@ -193,18 +194,18 @@ class EmployeeHibernateRepositoryTest {
         assertEquals(list.size(), 2);
 
         Employee employee = list.get(0);
-        assertEquals(employee.getName(), "Heather");
-        assertEquals(employee.getSurname(), "Paterson");
-        assertEquals(employee.getUsername(), "patheat");
-        assertEquals(employee.getPassword(), "$2y$05$TUfqZkJQ9hNpn6cVpwbnOOICCkji82jK4kFuVQXexkcsC60pH4ROq");
-        assertEquals(employee.getAuthorities().size(), 0);
+        assertEquals("Heather", employee.getName());
+        assertEquals("Paterson", employee.getSurname());
+        assertEquals("patheat", employee.getUsername());
+        assertTrue(passwordEncoder.matches("patheat", employee.getPassword()));
+        assertEquals(0, employee.getAuthorities().size());
 
         employee = list.get(1);
-        assertEquals(employee.getName(), "Heather");
-        assertEquals(employee.getSurname(), "Paterson");
-        assertEquals(employee.getUsername(), "pathea");
-        assertEquals(employee.getPassword(), "$2y$05$nOV.TLjdsSgLVKjFytgm4.L9f/lem06UxTabB9B6NnBVgoox8nn/.");
-        assertEquals(employee.getAuthorities().size(), 0);
+        assertEquals("Heather", employee.getName());
+        assertEquals("Paterson", employee.getSurname());
+        assertEquals("pathea", employee.getUsername());
+        assertTrue(passwordEncoder.matches("pathea", employee.getPassword()));
+        assertEquals(0, employee.getAuthorities().size());
 
 
     }
@@ -214,14 +215,14 @@ class EmployeeHibernateRepositoryTest {
     void findByUsername(){
         Employee employee = employeeRepository.find("baydan");
         assertNotNull(employee);
-        assertEquals(employee.getName(), "Daniel");
-        assertEquals(employee.getSurname(), "Bayne");
-        assertEquals(employee.getUsername(), "baydan");
-        assertEquals(employee.getPassword(), "$2y$05$L8IQDO993A5f/G/z7VjHm.XFwg4rdPCdUkTr/oTa0HaHSMPFQf9fu");
-        assertEquals(employee.getAuthorities().size(), 2);
+        assertEquals("Daniel", employee.getName());
+        assertEquals("Bayne", employee.getSurname());
+        assertEquals("baydan", employee.getUsername());
+        assertTrue(passwordEncoder.matches("baydan", employee.getPassword()));
+        assertEquals(2, employee.getAuthorities().size());
         List<GrantedAuthority> baydanAuth = new ArrayList<>(employee.getAuthorities());
-        assertEquals(baydanAuth.get(0).getAuthority(), "ROLE_ADMIN");
-        assertEquals(baydanAuth.get(1).getAuthority(), "ROLE_USER");
+        assertEquals("ROLE_ADMIN", baydanAuth.get(0).getAuthority());
+        assertEquals("ROLE_USER", baydanAuth.get(1).getAuthority());
     }
 
     @Test
@@ -258,7 +259,7 @@ class EmployeeHibernateRepositoryTest {
         employeeRepository.delete(employee);
         List<Employee> list = employeeRepository.find();
         assertFalse(list.contains(employee));
-        assertEquals(list.size(), 24);
+        assertEquals(24, list.size());
     }
 
     @Test
